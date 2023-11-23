6ce4b7af1852708f2f98d2fb40d06acc86d4767c
karlapa06 <A01705331@tec.mx>
2023-11-23 11:34:49 -0600

    Initial Commit

---
+++ CovidExamen/CovidExamenApp.swift
@@ -0,0 +1,17 @@
+//
+//  CovidExamenApp.swift
+//  CovidExamen
+//
+//  Created by KARLA PADILLA on 23/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CovidExamenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
